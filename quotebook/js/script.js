f0816044e4366cf1720055ee29f9d0309365d6d9
chinky23-tech <chinky0330@gmail.com>
2025-05-02 22:50:28 +0530

    added new features

---
+++ quotebook/js/script.js
@@ -0,0 +1,26 @@
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".quote-slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto-slide every 5 seconds
+setInterval(nextSlide, 5000);
+
+// Initial show
+showSlide(currentSlide);
+
